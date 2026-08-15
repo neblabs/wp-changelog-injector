5ed01836d418a4cb57603df55e81e06a3d458e54
rafark <rafarks@gmail.com>
2026-08-15 16:13:03 -0600

    updated installer and docs

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ curl -sSL  https://raw.githubusercontent.com/neblabs/wp-changelog-injector/main/
 sudo chmod +x "$targetFilePath"
 
 # warn if not in path
-if ! [[ "$targetDir" == *"/.local/bin"* ]]; then
+if ! [[ "$targetDir" == *"/.local/bin"*  ]]; then
     echo [warn] Installed to "$targetFilePath" but it "doesn't" seem to be in your PATH.
 fi
 
